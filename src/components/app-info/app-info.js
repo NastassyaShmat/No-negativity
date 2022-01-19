6a70c4e5c748325f31d10c4ecb433c7d1f402bad
Nastassia Shmat <yatsan.shmat@gmail.com>
2022-01-19 19:18:22 +0300

    fixed company name

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -3,7 +3,7 @@ import "./app-info.css";
 const AppInfo = ({onCountItems, onCountIncreased}) => {
   return (
     <div className="app-info">
-      <h1>Учет сотрудников в компании "No negative"</h1>
+      <h1>Учет сотрудников в компании "No negativity"</h1>
       <h2>Общее число сотрудников: {onCountItems}</h2>
       <h2>Премию получат: {onCountIncreased}</h2>
     </div>
